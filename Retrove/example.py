05ff69195c41a65a59be156f5b57d3c0c687218b
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2018-04-13 21:50:54 -0400

    PIL

--- Retrove/example.py
+++ Retrove/example.py
@@ -1,21 +1,13 @@
-from PIL import Image, ImageDraw
+from PIL import Image
+import random
 
+mylist = []
 
-class create_image():
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.image = Image.new(
-            "RGB", (self.width, self.height), (200, 200, 200))
-        self.size = self.image.size
+for i in range((40*40)):
+    color = (random.randrange(255), random.randrange(
+        255), random.randrange(255))
+    mylist.append(color)
 
-    def repetition(self, colors, rect):
-        self.image.paste(self.colors, self.rect)
-
-    def save(self):
-        self.image.save("wall.png")
-
-
-new_im = create_image(40, 40)
-
-new_im.repetition((55, 44, 200), [0, 0, new_im.size[0]/10, new_im.size[1]/10])
+img = Image.new('RGB', (40, 40))
+img.putdata(mylist)
+img.save('wall.png')
