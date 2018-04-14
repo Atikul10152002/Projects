fc8cb7888fd1381e29174da3e39d2840fab10f98
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2018-04-14 11:38:58 -0400

    commit #@@

--- Retrove/example.py
+++ Retrove/example.py
@@ -1,13 +1,43 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import random
+import sys
+import time
+import os
 
-mylist = []
+try:
+    repetition = int(sys.argv[1])
+except:
+    repetition = 100
 
-for i in range((40*40)):
-    color = (random.randrange(255), random.randrange(
-        255), random.randrange(255))
-    mylist.append(color)
+width = 455
+height = 809
+box_width = 10
+box_height = 10
+wall_num = 0
 
-img = Image.new('RGB', (40, 40))
-img.putdata(mylist)
-img.save('wall.png')
+for i in range(10):
+    im = Image.new('RGB', (width, height), color=(0, 0, 0))
+    # im.putdata(mylist)
+    draw = ImageDraw.Draw(im)
+
+    for i in range(1000):
+        x_val = random.triangular(-5, width)
+        y_val = random.triangular(-5, height)
+        x2_val = x_val + random.randrange(box_width)
+        y2_val = y_val + random.randrange(box_height)
+        draw.line([x_val, y_val, x2_val, y2_val],
+                  fill=(random.randrange(255), random.randrange(255), random.randrange(255)), width=1)
+    for i in range(repetition):
+        x_val = random.triangular(-5, width)
+        y_val = random.triangular(-5, height)
+        x2_val = x_val + random.randrange(box_width)
+        y2_val = y_val + random.randrange(box_height)
+        draw.polygon([random.choice([x_val, -x_val]), random.choice([y_val, -y_val]), random.choice([x2_val, -x2_val]), random.choice([y2_val, -y2_val]), random.choice([-y_val*2, y_val*2]), random.choice(
+            [-x_val*2]), random.choice([-y_val//2, y_val*2]), random.choice([-x_val//2])], fill=(round(random.randrange(0, 200)), round(random.randrange(0, 255)), round(random.randrange(0, 255))))
+    wall_num += 1
+    if not os.path.exists("walls"):
+        os.makedirs("walls")
+    filename = 'walls/wall{}.png'.format(wall_num)
+    print(wall_num)
+    im.save(filename)
+    # time.sleep(.1)
