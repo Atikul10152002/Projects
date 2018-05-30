876e54c6ee7641d0b92d401eb04d9b0a7bdc9228
Mohammad Islam <32369387+Atikul10152002@users.noreply.github.com>
2018-05-30 01:14:13 -0400

    rock paper scissor lizard spock in cpp
    took a freaking long time, but it's done now

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <ctime>
-#include <cstdlib>
-#include <cctype>
+import std;
+
 using namespace std;
 
+
 void game();
+string playerIfelse(string);
+string computerIfelse(int);
+string outcomeIfelse(string, string);
+
 
 int main ()
 {
@@ -23,27 +27,26 @@ int main ()
   - Lizard poisons Spock, eats Paper
   - Spock smashes Scissors, vaporizes Rock
 
-  # module that imports random integerd
+  # py modules 
   from random import randint
   import os
   import sys
   import time
   */
-  srand(time(0));
 
-  cout << "\n A GAME MADE BY MOHAMMAD ISLAM\n *The purpose of this program is to \
-  create a complicated game of Rock, Paper, Scissors, Lizard, Spock.\n * \
-  The game is made to be played against the COMPUTER. \n* The COMPUTER input is \
-  fully random \n\n\nThe winner is CHOSEN by the follwing rules \n- Rock blunts \
-  Scissors, and Lizard \n- Paper covers Rock, disproves Spock \n- Scissors cuts \
-  Paper, decapitates Lizard \n- Lizard piosons Spock, eats Paper \n- Spock smashes \
+  cout << "\n A GAME MADE BY MOHAMMAD ISLAM\n* The purpose of this program is to\
+  create a complicated game of Rock, Paper, Scissors, Lizard, Spock.\n* The\
+  game is made to be played against the COMPUTER. \n* The COMPUTER input is\
+  fully random \n\n\nThe winner is CHOSEN by the follwing rules \n- Rock blunts\
+  Scissors, and Lizard \n- Paper covers Rock, disproves Spock \n- Scissors cuts\
+  Paper, decapitates Lizard \n- Lizard piosons Spock, eats Paper \n- Spock smashes\
   Scissors, vaporizes Rock\n***\nPress \'q\' and \'Enter\' in any point to exit the game...\
   \n ___     ___     ___    \
   \n \\  \\    \\  \\    \\  \\   \
   \n  \\  \\    \\  \\    \\  \\  \
   \n   )  )    )  )    )  ) \
   \n  /  /    /  /    /  /  \
-  \n /__/    /__/    /__/ ";
+  \n /__/    /__/    /__/ " << endl;
 
   game();
 
@@ -51,127 +54,186 @@ int main ()
 
 
 // defination of the whole game
+static string stop = "n";
+
 void game()
 {
+  srand(time(0));
   // initial stop set to nothing
-  string stop = "n";
-  cout << stop;
-  string PLAYER;
+  string p_choice;
+  
     while (stop == "n"){
+      // stop = "y";
       // asks the player input
-      cout << "\n\n\nWhat do you want to play? \n[r]ock, [p]aper, [s]cissors, [l]izard, [sp]ock? >> ";
-      cin >> PLAYER;
+      cout << "\n\nWhat do you want to play? \n[r]ock, [p]aper, [s]cissors, [l]izard, [sp]ock? >> ";
+      cin >> p_choice;
       
-      if (PLAYER == "sp"){
-          PLAYER = "Spock";
-      }
-      else if (PLAYER == "p"){
-        PLAYER = "Paper";
-      }
-      else if (PLAYER == "l"){
-        PLAYER = "Lizard";
-      }
-      else if (PLAYER == "s"){
-        PLAYER = "Scissors";
-      }
-      else if (PLAYER == "r"){
-        PLAYER = "Rock";
-      }
-      else if (PLAYER == "q"){
+      if (p_choice != "q"){
+        string PLAYER = playerIfelse(p_choice);
+      
+        // computer random choise
+        string COMPUTER = computerIfelse(rand()%5);
+        
+        // Result
+        cout << "PLAYER" << " vs. " << "COMPUTER" << endl;
+        cout << PLAYER << " vs. " << COMPUTER << endl;
+      
+        // Outcomes
+        string OUTCOME = outcomeIfelse(PLAYER, COMPUTER);
+        cout << OUTCOME<< endl;
+
+        //Again 
+        string again_;
+        cout << "Do you want to try again? YES (y) or No (n)? >> ";
+        cin >> again_;
+        if (again_ == "n" || again_ == "q"){
+          cout << "\nTHANKS FOR TRYING OUT MY GAME...\n";
+          // stop = "y";
+          break;
+        } else {
+          cout << "...";
+        }
+
+      }else{
         break;
-      }
-      else if(PLAYER == "who"){
-        cout << "me" << endl;
-      }
-      else{
-        PLAYER = "invalid_input";
-      }
-      cout << PLAYER << endl;
-      stop = "y";
+        }
     }
-              // 
-//             # computer random choise
-//             CHOSEN = randint(1, 5)
-//             # print CHOSEN
-//             # computer inputs
-//             if CHOSEN == 1:
-//                 COMPUTER = "Rock"
-//             elif CHOSEN == 2:
-//                 COMPUTER = "Paper"
-//             elif CHOSEN == 3:
-//                 COMPUTER = "Scissors"
-//             elif CHOSEN == 4:
-//                 COMPUTER = "Lizard"
-//             else:
-//                 COMPUTER = "Spock"
-//             print (PLAYER, "vs", COMPUTER)
-//             time.sleep(1)
-//             # easter egg
-//             if PLAYER == "who":
-//                 print ("  _|_|          _|_|_|    _|_|    _|      _|  _|_|_|_|      _|      _|    _|_|    _|_|_|    _|_|_|_|      _|_|_|    _|      _| \n_|    _|      _|        _|    _|  _|_|  _|_|  _|            _|_|  _|_|  _|    _|  _|    _|  _|            _|    _|    _|  _|\n_|_|_|_|      _|  _|_|  _|_|_|_|  _|  _|  _|  _|_|_|        _|  _|  _|  _|_|_|_|  _|    _|  _|_|_|        _|_|_|        _|\n_|    _|      _|    _|  _|    _|  _|      _|  _|            _|      _|  _|    _|  _|    _|  _|            _|    _|      _|\n_|    _|        _|_|_|  _|    _|  _|      _|  _|_|_|_|      _|      _|  _|    _|  _|_|_|    _|_|_|_|      _|_|_|        _|    _|\n                                                                                                                            _|\n\n\n_|      _|    _|_|    _|    _|    _|_|    _|      _|  _|      _|    _|_|    _|_|_|        _|_|_|    _|_|_|  _|          _|_|    _|      _|\n_|_|  _|_|  _|    _|  _|    _|  _|    _|  _|_|  _|_|  _|_|  _|_|  _|    _|  _|    _|        _|    _|        _|        _|    _|  _|_|  _|_|\n_|  _|  _|  _|    _|  _|_|_|_|  _|_|_|_|  _|  _|  _|  _|  _|  _|  _|_|_|_|  _|    _|        _|      _|_|    _|        _|_|_|_|  _|  _|  _|\n_|      _|  _|    _|  _|    _|  _|    _|  _|      _|  _|      _|  _|    _|  _|    _|        _|          _|  _|        _|    _|  _|      _|\n_|      _|    _|_|    _|    _|  _|    _|  _|      _|  _|      _|  _|    _|  _|_|_|        _|_|_|  _|_|_|    _|_|_|_|  _|    _|  _|      _|  ")
-//             # outcomes
-//             elif PLAYER == COMPUTER:
-//                 print ("It\"s a DRAW!")
-//             elif PLAYER == "Rock" and COMPUTER == "Scissors":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Rock" and COMPUTER == "Paper":
-//                 print (str("COMPUTER wins!").upper())
-//             elif PLAYER == "Rock" and COMPUTER == "Lizard":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Rock" and COMPUTER == "Spock":
-//                 print (str("COMPUTER wins!").upper())
+}
 
-//             elif PLAYER == "Paper" and COMPUTER == "Rock":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Paper" and COMPUTER == "Scissors":
-//                 print (str("COMPUTER wins!").upper())
-//             elif PLAYER == "Paper" and COMPUTER == "Spock":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Paper" and COMPUTER == "Lizard":
-//                 print (str("COMPUTER wins!").upper())
+string playerIfelse(string PLAYER){
+  if (PLAYER == "sp"){
+    PLAYER = "Spock";
+  }
+  else if (PLAYER == "p"){
+    PLAYER = "Paper";
+  }
+  else if (PLAYER == "l"){
+    PLAYER = "Lizard";
+  }
+  else if (PLAYER == "s"){
+    PLAYER = "Scissors";
+  }
+  else if (PLAYER == "r"){
+    PLAYER = "Rock";
+  }
+  else if(PLAYER == "who"){
+    cout << "me" << endl;
+  }
+  else{
+    PLAYER = "invalid_input";
+  }
 
-//             elif PLAYER == "Scissors" and COMPUTER == "Paper":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Scissors" and COMPUTER == "Rock":
-//                 print (str("COMPUTER wins!").upper())
-//             elif PLAYER == "Scissors" and COMPUTER == "Lizard":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Scissors" and COMPUTER == "Spock":
-//                 print (str("COMPUTER wins!").upper())
+  return PLAYER;
+}
 
-//             elif PLAYER == "Lizard" and COMPUTER == "Paper":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Lizard" and COMPUTER == "Rock":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Lizard" and COMPUTER == "Scissors":
-//                 print (str("COMPUTER wins!").upper())
-//             elif PLAYER == "Lizard" and COMPUTER == "Spock":
-//                 print (str("COMPUTER wins!").upper())
+string computerIfelse(int CHOSEN){
+  string COMPUTER;
+  if (CHOSEN == 1){
+    COMPUTER = "Rock";
+  }
+  else if (CHOSEN == 2){
+    COMPUTER = "Paper";
+  }   
+  else if (CHOSEN == 3){
+    COMPUTER = "Scissors";
+  }     
+  else if (CHOSEN == 4){
+    COMPUTER = "Lizard";
+  }
+  else{
+    COMPUTER = "Spock";
+  }
+  return COMPUTER;
+}
 
-//             elif PLAYER == "Spock" and COMPUTER == "Paper":
-//                 print (str("COMPUTER wins!").upper())
-//             elif PLAYER == "Spock" and COMPUTER == "Rock":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Spock" and COMPUTER == "Scissors":
-//                 print (str("PLAYER wins!").upper())
-//             elif PLAYER == "Spock" and COMPUTER == "Lizard":
-//                 print (str("COMPUTER wins!").upper())
-//             else:
-//                 print ("Invalid user input, COMPUTER wins!")
-//             time.sleep(1)
+string outcomeIfelse(string PLAYER, string COMPUTER){
+  string OUTCOME;
+  if (PLAYER == "who"){
+    OUTCOME = "\
+        _|_|          _|_|_|    _|_|    _|      _|  _|_|_|_|      _|      _|    _|_|    _|_|_|    _|_|_|_|      _|_|_|    _|      _| \n\
+      _|    _|      _|        _|    _|  _|_|  _|_|  _|            _|_|  _|_|  _|    _|  _|    _|  _|            _|    _|    _|  _| \n\
+      _|_|_|_|      _|  _|_|  _|_|_|_|  _|  _|  _|  _|_|_|        _|  _|  _|  _|_|_|_|  _|    _|  _|_|_|        _|_|_|        _| \n\
+      _|    _|      _|    _|  _|    _|  _|      _|  _|            _|      _|  _|    _|  _|    _|  _|            _|    _|      _| \n\
+      _|    _|        _|_|_|  _|    _|  _|      _|  _|_|_|_|      _|      _|  _|    _|  _|_|_|    _|_|_|_|      _|_|_|        _|    _| \n\
+                                                                                                                                  _| \n\
+      _|      _|    _|_|    _|    _|    _|_|    _|      _|  _|      _|    _|_|    _|_|_|        _|_|_|    _|_|_|  _|          _|_|    _|      _| \n\
+      _|_|  _|_|  _|    _|  _|    _|  _|    _|  _|_|  _|_|  _|_|  _|_|  _|    _|  _|    _|        _|    _|        _|        _|    _|  _|_|  _|_| \n\
+      _|  _|  _|  _|    _|  _|_|_|_|  _|_|_|_|  _|  _|  _|  _|  _|  _|  _|_|_|_|  _|    _|        _|      _|_|    _|        _|_|_|_|  _|  _|  _| \n\
+      _|      _|  _|    _|  _|    _|  _|    _|  _|      _|  _|      _|  _|    _|  _|    _|        _|          _|  _|        _|    _|  _|      _| \n\
+      _|      _|    _|_|    _|    _|  _|    _|  _|      _|  _|      _|  _|    _|  _|_|_|        _|_|_|  _|_|_|    _|_|_|_|  _|    _|  _|      _|  ";
+  }
 
-//         stop = input(
-//             "\n\nDo you want to try again? YES (y) or No (n)?  ")
-//         if ("n" in stop.lower()) or ("q" in stop.lower()) or ("y" in stop.lower()):
-//             if ("n" in stop.lower()) or ("q" in stop.lower()):
-//                 time.sleep(1)
-//                 print ('\nTHANKS FOR TRYING OUT MY GAME...\n')
-//                 time.sleep(1)
-//                 sys.exit()
-//             else:
-//                 os.system('clear')
-//                 print ("...")
-//         else:
-//             print ("Sorry your input was counted as no!")
-//             sys.exit()
+  // outcomes
+  else if (PLAYER == COMPUTER){
+    OUTCOME = "It\"s a DRAW!";
+  }
+  else if (PLAYER == "Rock" && COMPUTER == "Scissors"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Rock" && COMPUTER == "Paper"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else if (PLAYER == "Rock" && COMPUTER == "Lizard"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Rock" && COMPUTER == "Spock"){
+    OUTCOME = "COMPUTER wins!";
+  }
+
+  else if (PLAYER == "Paper" && COMPUTER == "Rock"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Paper" && COMPUTER == "Scissors"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else if (PLAYER == "Paper" && COMPUTER == "Spock"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Paper" && COMPUTER == "Lizard"){
+    OUTCOME = "COMPUTER wins!";
+  }
+
+  else if (PLAYER == "Scissors" && COMPUTER == "Paper"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Scissors" && COMPUTER == "Rock"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else if( PLAYER == "Scissors" && COMPUTER == "Lizard"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Scissors" && COMPUTER == "Spock"){
+    OUTCOME = "COMPUTER wins!";
+  }
+
+  else if (PLAYER == "Lizard" && COMPUTER == "Paper"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Lizard" && COMPUTER == "Rock"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Lizard" && COMPUTER == "Scissors"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else if (PLAYER == "Lizard" && COMPUTER == "Spock"){
+    OUTCOME = "COMPUTER wins!";
+  }
+
+  else if (PLAYER == "Spock" && COMPUTER == "Paper"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else if (PLAYER == "Spock" && COMPUTER == "Rock"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Spock" && COMPUTER == "Scissors"){
+    OUTCOME = "PLAYER wins!";
+  }
+  else if (PLAYER == "Spock" && COMPUTER == "Lizard"){
+    OUTCOME = "COMPUTER wins!";
+  }
+  else{
+    OUTCOME = "Invalid user input, COMPUTER wins!";
+  }
+
+  return OUTCOME;
 }
