0e73523989a0bda3cefff8ccb64cf7cbb2c988fd
Mohammad Islam <Atikul10152002@gmail.com>
2018-08-12 02:12:32 -0400

    asdf

--- py/random_projects/_email.py
+++ py/random_projects/_email.py
@@ -1,11 +1,14 @@
 import smtplib
+from sys import argv
 from email.mime.text import MIMEText
 
+
+
 myEmail = 'resatikul@gmail.com'
-sendEmail = 'atikul10152002@gmail.com'
+sendEmail = argv[1]
 # msg = "THIS IS WORKING"
-msg = MIMEText("THIS IS WORKING")
-msg['subject'] = 'Our family reunion'
+msg = MIMEText(argv[3])
+msg['subject'] = argv[2]
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
