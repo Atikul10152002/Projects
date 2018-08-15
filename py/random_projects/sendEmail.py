25f713dd04122b72f9b0dead6bfbac3f7d7196ed
Mohammad Islam <Atikul10152002@gmail.com>
2018-08-15 00:31:20 -0400

    ss

--- py/random_projects/sendEmail.py
+++ py/random_projects/sendEmail.py
@@ -4,9 +4,12 @@ from email.mime.text import MIMEText
 from getpass import getpass
 
 
+# myEmail = 'resatikul'
 myEmail = input("Your email: ")
+# myPass = 'Islam2002@'
 myPass = str(getpass("Your password: "))
-sendEmail = input("Recipient's email: ")
+sendEmail = str(input("Recipient's email: "))
+
 
 # Message
 if '@gmail.com' not in myEmail:
@@ -18,7 +21,7 @@ subject = input("Email Subject: ")
 msg = MIMEText(input("Email Body: "))
 msg['subject'] = subject
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server = smtplib.SMTP_SSL('smtp.gmail.com')
 server.login(myEmail, myPass)
 server.sendmail(myEmail, [sendEmail], msg.as_string())
 server.quit()
