3c076e6f0879a47f2f851ac67f5176c75beb1e4d
Atikul <atikul@Raspberry-PI--6675636b.local>
2018-10-07 15:34:56 -0400

    fd

--- py/fractal/turtle_first.py
+++ py/fractal/turtle_first.py
@@ -1,23 +1,28 @@
-import turtle
+import turtle as d
+from cairosvg import svg2png
 import canvasvg
 
-d = turtle.Turtle()
 d.shape("turtle")
+d.screensize(2000, 1500)
 d.speed(0)
 d.pu()
 d.goto(x=0,y=-100)
 d.pd()
 
 angle = 0
+increase = 1
 while 1:
+    
     d.circle(200)
-    d.left(1)
-    angle += 1
+    d.left(increase)
+    angle += increase
+    print(angle)
     if angle >= 360:
         break
+
 
 d.hideturtle()
 canv = d.getscreen().getcanvas()
 canvasvg.saveall("turtle.svg", canv)
-
-turtle.done()
+exit()
+d.done()
